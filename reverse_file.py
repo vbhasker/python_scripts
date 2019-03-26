5e75eb3ab6e55ff77b33863dc1191c4171ac33a4
vbhasker <vinodh.ab@gmail.com>
2019-03-25 21:49:45 -0500

    Added the script for arg parse

--- reverse_file.py
+++ reverse_file.py
@@ -1,3 +1,20 @@
+#!/usr/bin/env python3
+
 from argparse import ArgumentParser
 
 parser = ArgumentParser(description='Reverse the content of the file')
+parser.add_argument('filename', help='file to read')
+parser.add_argument('--limit', '-l', type=int, help='number of lines to read')
+parser.add_argument('--version', '-v', action='version', version='%(prog)s 1.0')
+
+args = parser.parse_args()
+
+with open(args.filename) as f:
+    lines = f.readlines()
+    lines.reverse()
+
+    if args.limit:
+        lines = lines[:args.limit]
+
+    for line in lines:
+        print(line.strip()[::-1])
